5a25c6c803ff943c36b833c54426d12f5d052d27
Mehmet Basrioğlu <99546413+mhmtbsrglu@users.noreply.github.com>
2022-12-11 23:58:22 +0300

    Feature/XML_JSON (#3)
    * Development branch'i ve service katmanı oluşturuldu.
    
    * 10.12.2022
    Api versiyonlaması yapıldı ve Query parametreleri servise uyarlandı.
    
    * 10.12.2022
    GlobalExceptionHandler sınıfı oluşturuldu ve runtime exceptionların yakalanması sağlandı.
    
    * 10.12.2022
    GlobalExceptionHandler sınıfı oluşturuldu ve runtime exceptionların yakalanması sağlandı.
    
    * 11.12.2022
    XML VE Rest Api oluşturuldu ardından exceptionların yakalanması sağlandı.

--- src/main/java/com/mernis/wsdlclient/api/MernisController.java
+++ src/main/java/com/mernis/wsdlclient/api/MernisController.java
@@ -3,12 +3,16 @@ package com.mernis.wsdlclient.api;
 import com.mernis.wsdlclient.annotation.ApiV1;
 import com.mernis.wsdlclient.services.base.BaseMernis;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mernis.wsdlclient.tr.gov.nvi.tckimlik.ws.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
@@ -21,14 +25,24 @@ public class MernisController {
 	@Autowired
 	BaseMernis baseMernis;
 	
-	@GetMapping("/validate")
+	@GetMapping(value="/validate",produces="application/json")
 	public Map<String,Object> mernisDogrula(@RequestParam("tc") String tc,
 											@RequestParam("ad") String ad,
 											@RequestParam("soyAd") String soyAd,
 											@RequestParam("dogumYili") String dogumYili
-											) {
+											) throws IOException, ParserConfigurationException, SAXException {
 
 		return baseMernis.validateMe(tc,ad,soyAd,dogumYili);
+	}
+
+	@GetMapping(value = "/validate/xml", produces = {"application/xml"}, consumes = {"application/xml"})
+	public Map<String,Object> mernisDogrulaXML(@RequestParam("tc") String tc,
+											@RequestParam("ad") String ad,
+											@RequestParam("soyAd") String soyAd,
+											@RequestParam("dogumYili") String dogumYili
+	) throws IOException, ParserConfigurationException, SAXException {
+		Map<String,Object> response = baseMernis.validateMe(tc,ad,soyAd,dogumYili);
+		return response;
 	}
 
 
--- src/main/java/com/mernis/wsdlclient/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/mernis/wsdlclient/handlers/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.mernis.wsdlclient.handlers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +20,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleRuntimeException(RuntimeException exception){
         Map<String,Object> response = new HashMap<>();
         response.put("message",exception.getMessage());
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException exception){
+        Map<String,Object> response = new HashMap<>();
+        response.put("message","Content-type required.");
+        response.put("listOfContentTypes", Arrays.asList("application/xml","application/json"));
         return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
 }
